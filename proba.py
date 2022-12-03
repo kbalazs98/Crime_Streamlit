4e4c9c151745117de62ce8fc250e9d02b9cb4aba
koko yx <karmi.balazs1@gmail.com>
2022-12-03 17:01:38 +0100

    default values

--- proba.py
+++ proba.py
@@ -24,14 +24,14 @@ def main():
     st.markdown(html_temp, unsafe_allow_html = True)
 
     #'population','numbUrban','medIncome','NumUnderPov','NumKidsBornNeverMar','NumImmig','NumInShelters','NumStreet'
-    population = st.number_input("population",1)
-    numbUrban = st.number_input("numbUrban",1)
-    medIncome = st.number_input("medIncome",1)
-    NumUnderPov = st.number_input("NumUnderPov",1)
-    NumKidsBornNeverMar = st.number_input("NumKidsBornNeverMar",1)
-    NumImmig = st.number_input("NumImmig",1)
-    NumInShelters = st.number_input("NumInShelterst",1)
-    NumStreet = st.number_input("NumStreet",1)
+    population = st.number_input("population",7322564)
+    numbUrban = st.number_input("numbUrban",7322564)
+    medIncome = st.number_input("medIncome",29823)
+    NumUnderPov = st.number_input("NumUnderPov",1384994)
+    NumKidsBornNeverMar = st.number_input("NumKidsBornNeverMar",527557)
+    NumImmig = st.number_input("NumImmig",2082931)
+    NumInShelters = st.number_input("NumInShelterst",23383)
+    NumStreet = st.number_input("NumStreet",10447)
 
     params = [[population,numbUrban,medIncome,NumUnderPov,NumKidsBornNeverMar,NumImmig,NumInShelters,NumStreet]]
     robbs = predict_robbery(params)
